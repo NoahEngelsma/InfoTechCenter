b7fe7404c1fa3f038de5b980f1a1c036891091c0
NoahEngelsma <engelsmanoah@gmail.com>
2025-02-10 13:50:40 -0500

    Created VRS Function - Snowing Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,14 +1,22 @@
 print("\n***************************************************\n")
 
-print("\tWeather Branch - Developer: Noah Engelsma")
+print("Weather Branch - Developer: Noah Engelsma\n")
 
 #Import Libaries Here!
 import random
 from time import sleep
+
 #Weather function to determine the weather
 def weather():
     weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
     weatherCondition = random.choice(weatherForecastList)
     return weatherCondition
 
-print (weather())
+weatherAlert = weather()
+
+def vehicleResponseSystem():
+    if weatherAlert == "snowing":
+        print("The National Weather Service has updated your alarm by 30 minutes because"
+        " it is", weatherAlert, "outside.")
+
+vehicleResponseSystem()    
