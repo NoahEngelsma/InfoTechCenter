0859128f74dbcca0953b78e041ddd2ca975300cb
NoahEngelsma <engelsmanoah@gmail.com>
2025-02-11 13:49:44 -0500

    ChatGPT - Efficient Code - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,55 +1,48 @@
+import random  # For random weather selection
+from time import sleep  # For potential delays
+
+
 # Print a decorative line for better output formatting
-print("\n***************************************************\n")
+def print_separator():
+    print("\n" + "*" * 51 + "\n")
+
 
 # Display program title and developer name
-print("Weather Branch - Developer: Noah Engelsma\n")
+def display_title():
+    print("Weather Branch - Developer: Noah Engelsma\n")
 
-# Import necessary libraries
-import random  # Used to randomly select weather conditions
-from time import sleep  # (Not used in this program but could be for delays)
+
 # Function to randomly determine the weather condition
-def weather():
-    # List of possible weather conditions
-    weatherForecastList = ["snowing", "blizzard", "icy", "rainy", "windy", "sunny"]
-    # Randomly select a weather condition from the list
-    weatherCondition = random.choice(weatherForecastList)
-    return weatherCondition  # Return the selected weather condition
-# Call the weather function to get the current weather alert
-weatherAlert = weather()
-# Function to determine the vehicle response based on the weather condition
-def vehicleResponseSystem():
-    if weatherAlert == "snowing":
-        print("The National Weather Service has updated your alarm by 30 minutes because"
-        " it is", weatherAlert, "outside.")
-        sleep(1)
-        print("VRS has been engaged only allowing us to drive 55MPH.")
-    elif weatherAlert == "blizzard":
-        print("The National Weather Service has updated your alarm by 60 minutes because"
-        " it is a", weatherAlert, "outside!")
-        sleep(1)
-        print("VRS has been engaged only allowing us to drive 45MPH.")
-    elif weatherAlert == "icy":
-        print("The National Weather Service has updated your alarm by 90 minutes because"
-        " it is", weatherAlert, "outside!")
-        sleep(1)
-        print("VRS has been engaged only allowing us to drive 35MPH.")
-    elif weatherAlert == "rainy":
-        print("The National Weather Service has updated your alarm by 10 minutes because"
-        " it is", weatherAlert, "outside!")
-        sleep(1)
-        print("VRS has been engaged only allowing us to drive 65MPH.")
-    elif weatherAlert == "windy":
-        print("The National Weather Service has updated your alarm by 5 minutes because"
-        " it is", weatherAlert, "outside!")
-        sleep(1)
-        print("VRS has been engaged only allowing us to drive 70MPH.")
-    else:
-        print("The National Weather Service is calling"
-        " for", weatherAlert, "skies. Have a good day!")
-        sleep(1)
-        print("VRS has been disengaged")
+def get_weather():
+    return random.choice(["snowing", "blizzard", "icy", "rainy", "windy", "sunny"])
 
-# Call the vehicleResponseSystem function to display the alert message
-vehicleResponseSystem()
-# Print a closing decorative line for better output formatting
-print("\n***************************************************\n")
+
+# Function to determine the vehicle response based on the weather condition
+def vehicle_response_system(weather_alert):
+    responses = {
+        "snowing": (30, 55),
+        "blizzard": (60, 45),
+        "icy": (90, 35),
+        "rainy": (10, 65),
+        "windy": (5, 70),
+    }
+
+    if weather_alert in responses:
+        delay, speed = responses[weather_alert]
+        print(
+            f"The National Weather Service has updated your alarm by {delay} minutes because it is {weather_alert} outside.")
+        sleep(1)
+        print(f"VRS has been engaged only allowing us to drive {speed}MPH.")
+    else:
+        print(f"The National Weather Service is calling for {weather_alert} skies. Have a good day!")
+        sleep(1)
+        print("VRS has been disengaged.")
+
+
+# Main program execution
+if __name__ == "__main__":
+    print_separator()
+    display_title()
+    weather_alert = get_weather()
+    vehicle_response_system(weather_alert)
+    print_separator()
