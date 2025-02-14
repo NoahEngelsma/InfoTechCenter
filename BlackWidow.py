a24b1640ae00772f0ea08e1b950b9c34e308b2a4
NoahEngelsma <engelsmanoah@gmail.com>
2025-02-14 13:53:14 -0500

    Added Libaries & gasLevelGaugeFunction - NOT TESTED - Program in Github

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,3 +1,11 @@
 print("\n***************************************\n")
 print("Gasoline Branch - Developer: Noah Engelsma\n")
 
+
+import random
+from time import sleep
+
+def gasLevelGauge():
+  gasLevelList = ["Empty", :"Low", "Quarter", "Half", "Three Quarter", "Full"]
+  return random.choice(gasLevelList)
+  
