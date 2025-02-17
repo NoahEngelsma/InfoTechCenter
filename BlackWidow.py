77d20a490e349969f066ae9e2ec3bf13001ed524
NoahEngelsma <engelsmanoah@gmail.com>
2025-02-17 13:54:25 -0500

    Added GasLevelAlertFunction - Created Variables through QuarterTank - Stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,7 +1,6 @@
 print("\n***************************************\n")
 print("Gasoline Branch - Developer: Noah Engelsma\n")
 
-
 import random
 from time import sleep
 
@@ -13,8 +12,21 @@ def gasStations():
         gasStationsList = ["Shell", "Marathon", "Speedway", "Circle K", "Wesco", "7/11", "Meijer", "Buc-ees"]
         return random.choice(gasStationsList)
 
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(1,25),1)
+    milesToGasStationQuarterTank = round(random.uniform(25.1,50),1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty":
+        print("***WARNING YOU ARE OUT OF GAS***\n")
+        sleep(1.25)
+        print("Calling AAA")
+    elif gasLevelIndicator == "Low":
+        print("***WARNING YOU ARE LOW ON GAS CHECKING GPS FOR CLOSEST GAS STATION***\n")
+        sleep(1.25)
+        print("The closest Gas Station is ", gasStations(), "which is", milesToGasStationLow, "miles away.\n")
+    elif gasLevelIndicator == "Quarter":
+        print("***WARNING YOU ARE ALMOST OUT OF GAS CHECKING FOR CLOSEST GAS STATION***\n")
+        sleep(1.25)
+        print("The closest Gas Station is", gasStations(), "which is", milesToGasStationQuarterTank, "miles away.\n")
 
-
-
-print(gasLevelGauge())
-print(gasStations())
+gasLevelAlert()
